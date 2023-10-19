5854a0effe85d8db17860725cc03534189b4aa8a
cpierres <cpierres@hotmail.com>
2023-10-19 10:31:19 +0200

    Type Generic pour Class : KeyValueInfo<K, V, I> extends KeyValue<K, V>

---
+++ src/exo/generics/KeyValueInfo.java
@@ -0,0 +1,23 @@
+package exo.generics;
+
+public class KeyValueInfo<K, V, I> extends KeyValue<K, V> {
+
+    private I info;
+
+    public KeyValueInfo(K key, V value) {
+        super(key, value);
+    }
+
+    public KeyValueInfo(K key, V value, I info) {
+        super(key, value);
+        this.info = info;
+    }
+
+    public void setInfo(I info) {
+        this.info = info;
+    }
+
+    public I getInfo() {
+        return info;
+    }
+}
