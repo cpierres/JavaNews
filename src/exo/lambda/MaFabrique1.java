b1edad243dafe510e9d1a51c697d352f6b00b25a
cpierres <cpierres@hotmail.com>
2023-10-23 11:48:20 +0200

    référence à un constructeur : possible de préciser le ou les types si la classe est typée avec un generic (voir MaFabrique1)

---
+++ src/exo/lambda/MaFabrique1.java
@@ -0,0 +1,6 @@
+package exo.lambda;
+
+@FunctionalInterface
+public interface MaFabrique1<T> {
+    T creerInstance();
+}
---
+++ src/exo/lambda/TestConstructeurReferenceGeneric1.java
@@ -0,0 +1,13 @@
+package exo.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestConstructeurReferenceGeneric1 {
+    public static void main(String[] args) {
+        //possible de préciser le ou les types si la classe est typée avec un generic (voir MaFabrique1)
+        MaFabrique1<List<String>> fabrique1 = ArrayList<String>::new;
+        List<String> mesChaines = fabrique1.creerInstance();
+        mesChaines.forEach(x -> System.out.println(x));//rien dans le tableau
+    }
+}
