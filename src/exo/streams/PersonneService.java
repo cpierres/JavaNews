1a382686135671f61128f8fec4aae1456d651327
cpierres <cpierres@hotmail.com>
2023-10-26 20:20:22 +0200

    correctifs mineurs

--- src/exo/streams/PersonneService.java
+++ src/exo/streams/PersonneService.java
@@ -1,11 +1,8 @@
 package exo.streams;
 
-import java.util.Random;
-
 import exo.lambda.Personne;
 
 public class PersonneService {
-    private static Random random = new Random();
 
     /**
      * En fait cette méthode de demo génère une personne dont on veut
