b307a4a7eb8185f4d15cb97c5716ecbad442dd9d
cpierres <cpierres@hotmail.com>
2023-10-27 15:00:42 +0200

    Stream : reduce pour concaténation - autres exemples

--- src/exo/streams/StreamReducePourConcat.java
+++ src/exo/streams/StreamReducePourConcat.java
@@ -6,6 +6,19 @@ public class StreamReducePourConcat {
     public static void main(String[] args) {
         String lettres = Stream.of("a", "b", "c", "d")
                 .reduce((accumulator, item) -> accumulator + ", " + item).orElse("");
-        System.out.println(lettres);
+        System.out.println(lettres);//a, b, c, d
+
+        // attention, si valeur initiale, elle s'ajoute
+        String lettres2 = Stream.of("a", "b", "c", "d")
+                .reduce("", (accumulator, item) -> accumulator + ", " + item);
+        System.out.println(lettres2);//, a, b, c, d
+
+        // Si le Stream ne contient aucun élément, alors c'est la valeur initiale qui
+        // est renvoyée.
+        String lettres3 = Stream.of("a", "b", "c", "d")
+                           .limit(0)
+                           .reduce("X", (accumulator, item) -> accumulator + ", " + item);
+    System.out.println(lettres3);// X
+
     }
 }
