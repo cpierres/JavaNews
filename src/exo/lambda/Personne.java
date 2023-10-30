f3bc21ddebe5261a601ae18b61cf2b13160cba08
cpierres <cpierres@hotmail.com>
2023-10-30 11:43:53 +0100

    Stream : collectToXxx

--- src/exo/lambda/Personne.java
+++ src/exo/lambda/Personne.java
@@ -61,4 +61,41 @@ public class Personne {
     public String toString() {
         return "Personne [nom=" + nom + ", prenom=" + prenom  + ", genre=" + genre + ", taille=" + taille + "]";
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nom == null) ? 0 : nom.hashCode());
+        result = prime * result + ((prenom == null) ? 0 : prenom.hashCode());
+        result = prime * result + taille;
+        result = prime * result + ((genre == null) ? 0 : genre.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Personne other = (Personne) obj;
+        if (nom == null) {
+            if (other.nom != null)
+                return false;
+        } else if (!nom.equals(other.nom))
+            return false;
+        if (prenom == null) {
+            if (other.prenom != null)
+                return false;
+        } else if (!prenom.equals(other.prenom))
+            return false;
+        if (taille != other.taille)
+            return false;
+        if (genre != other.genre)
+            return false;
+        return true;
+    }
 }
